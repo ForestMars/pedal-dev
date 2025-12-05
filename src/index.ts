79f9fb61750f2228c413e75438de996ee656d106
ForestMars <themarsgroup+gh@gmail.com>
2025-12-04 22:05:38 -0500

    remove test bug

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import { Probot } from 'probot';
 import { ConfigLoader } from './config-loader';
 import { ReviewEngine } from './review-engine';
 
-const x = undefined.y;
-
 export default (app: Probot) => {
   // 1. Instantiate ConfigLoader once at the start
   const configLoader = new ConfigLoader();
